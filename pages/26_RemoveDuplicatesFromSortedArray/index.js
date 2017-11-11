e0f5df8d32f1247541cb89e45eda037c6b6cd1fb
walker <walker.study@foxmail.com>
2017-11-11 22:32:05 +0800

    init Glock

---
+++ pages/26_RemoveDuplicatesFromSortedArray/index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    for(var i = nums.length - 1; i > 0; i--){
+        if(nums[i] === nums[i-1]){
+            nums.splice(i, 1);
+        }
+    }
+    return nums.length;
+};
+
+console.log(removeDuplicates([1,1,2]));
